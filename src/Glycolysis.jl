31fc60fb9e89e7f6f8ee03d31f613a11204fa59f
May Ben hamo <maybenhamo@Mays-MBP.lan>
2023-12-09 14:05:18 +0200

    prepare package structure

---
+++ src/Glycolysis.jl
@@ -0,0 +1,13 @@
+module 
+
+include("PKM2_analysis_figure_outlier.jl")
+include("PKM2_analysis_model_selection.jl")
+include("PKM2_analysis_various_plots.jl")
+include("PKM2_bootstrap.jl")
+include("PKM2_fig_loocv.jl")
+include("PKM2_fitting_functions.jl")
+include("PKM2_forward_model_selection.jl")
+include("PKM2_model_selection_update.jl")
+include("PKM2_reverse_model_selection.jl")
+
+end # module Glycolysis
